b58ecc6e89dbbdd421db6a5c3a4a9e10d1afca1d
Ryan Gorgol <ryangorgol@gmail.com>
2022-07-13 11:42:14 -0700

    removed vh calculation to store in custom properties

--- pages/index.tsx
+++ pages/index.tsx
@@ -110,7 +110,7 @@ export async function getStaticProps() {
 
 const S_Main = styled.main`
   width: 100vw;
-  height: calc(var(--vh) * 100);
+  height: 100vh;
   overflow-x: hidden; 
   overflow-y: auto;
   background: var(--black);
