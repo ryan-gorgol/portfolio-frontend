b4eb1eda4001a9f9adcd193aa136cd8699ee7fba
Ryan Gorgol <ryangorgol@gmail.com>
2022-07-13 12:22:05 -0700

    vw to documentElement.clientWidth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   if (typeof window !== "undefined") {
     let vh = document.documentElement.clientHeight * 0.01;
-    let vw = document.body.clientWidth * 0.01;
+    let vw = document.documentElement.clientWidth * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
     document.documentElement.style.setProperty('--vw', `${vw}px`);
     console.log(vh, 'vh', vw, 'vw')
