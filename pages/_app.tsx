1bdba975a88e037d7a7904e4544e85629e0a7d36
Ryan Gorgol <ryangorgol@gmail.com>
2022-07-13 11:59:47 -0700

    instituted a css custom prop being set on page load via document.body.clientWidth in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,12 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+
+  if (typeof window !== "undefined") {
+    let vh = document.body.clientWidth * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    console.log(vh, 'vh')
+  }
   
   return (
     <>
