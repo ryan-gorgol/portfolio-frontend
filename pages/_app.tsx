63c951374745976696dc4adae57daf261e9566cd
Ryan Gorgol <ryangorgol@gmail.com>
2022-07-13 11:53:16 -0700

    oops, forgot to commit _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,6 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-
-// calculate view height and set --vh css custom property with result
-  if (typeof window !== "undefined") {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-    console.log(vh, 'vh')
-  }
   
   return (
     <>
