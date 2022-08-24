14bcd27c144737cc2bf3ff2b2dddd854b1c0f448
Ryan Gorgol <ryangorgol@gmail.com>
2022-08-24 14:26:35 -0700

    added padding to create proper spacing

--- pages/theodinproject/index.tsx
+++ pages/theodinproject/index.tsx
@@ -19,6 +19,7 @@ const index = () => {
           The Odin Project is one of those &quot;What I wish I had when I was learning&quot; resources. Not everyone has access to a computer science education or the funds to attend an intensive coding school and neither of those is right for everyone anyway. This project is designed to fill in the gap for people who are trying to hack it on their own but still want a high quality education.
         </S_H2>
       </S_Heading>
+      <S_Space />
       <S_LinkContainer>
           {
           links.map((link, index) => (
@@ -54,6 +55,10 @@ const S_H1 = styled.h1`
 const S_H2 = styled.h2`
   color: white;
   font-weight: 200;
+`
+
+const S_Space = styled.div`
+  height: 8rem;
 `
 
 const S_LinkContainer = styled.div`
