18901c34cb141ce39f267902a24f3d27425953e1
Ryan Gorgol <ryangorgol@gmail.com>
2022-08-24 14:43:58 -0700

    added interactive columns for the three choices available

--- pages/theodinproject/rockpaperscissors.tsx
+++ pages/theodinproject/rockpaperscissors.tsx
@@ -4,10 +4,23 @@ const rockpaperscissors = () => {
   return (
     <S_Section>
       <S_H1>Rock Paper Scissors</S_H1>
-      <S_Button>It was :smile </S_Button>
+      <S_PlayingBoard>
+        <S_Choices>
+          <S_H2>Human</S_H2>
+          <S_Button>👊</S_Button>
+          <S_Button>✋</S_Button>
+          <S_Button>✌️</S_Button>
+        </S_Choices>
+        <S_Choices>
+          <S_H2>Computer</S_H2>
+          <S_Button>👊</S_Button>
+          <S_Button>✋</S_Button>
+          <S_Button>✌️</S_Button>
+        </S_Choices>
+      </S_PlayingBoard>
     </S_Section>
   )
-}
+} 
 
 export default rockpaperscissors
 
@@ -20,6 +33,27 @@ const S_H1 = styled.h1`
   font-size: 4rem;
 `
 
+const S_H2 = styled.h2`
+  color: white;
+  font-weight: 300;
+  font-size: 2rem;
+`
+
+const S_PlayingBoard = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const S_Choices = styled.div`
+  width: 150px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
 const S_Button = styled.button`
-  
+  height: 4rem;
+  width: 4rem;
+  font-size: 3rem;
 `
