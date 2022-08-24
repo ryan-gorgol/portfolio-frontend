d3e6c74d297e707fd3f2a96f66e0e6939e2a7888
Ryan Gorgol <ryangorgol@gmail.com>
2022-08-24 14:34:57 -0700

    adding emoji support

--- pages/theodinproject/rockpaperscissors.tsx
+++ pages/theodinproject/rockpaperscissors.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import styled from 'styled-components'
 
 const rockpaperscissors = () => {
   return (
-    <h1>Rock Paper Scissors</h1>
+    <S_Section>
+      <S_H1>Rock Paper Scissors</S_H1>
+      <S_Button>It was :smile </S_Button>
+    </S_Section>
   )
 }
 
 export default rockpaperscissors
+
+const S_Section = styled.section`
+  padding: 1rem;
+`
+
+const S_H1 = styled.h1`
+  color: white;
+  font-size: 4rem;
+`
+
+const S_Button = styled.button`
+  
+`
