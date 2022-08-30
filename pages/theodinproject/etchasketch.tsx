da50fb3cee564921992213880071b119ea277767
Ryan Gorgol <ryangorgol@gmail.com>
2022-08-30 11:00:17 -0700

    preliminary page works as expected. styling needs to be defined and refactored.

--- pages/theodinproject/etchasketch.tsx
+++ pages/theodinproject/etchasketch.tsx
@@ -1,6 +1,5 @@
 import styled from 'styled-components'
 import { useState, useEffect } from 'react'
-import handler from '../api/hello'
 
 interface Props {
   grid: number
@@ -33,31 +32,64 @@ const Etchasketch = () => {
   }
 
   return (
-    <>
+    <S_Page>
       <h1>etchasketch</h1>
-      <S_GridContainer grid={grid.length}>
-        {
-          grid.map((index) => <div key={index} onMouseEnter={(e) => changeBackground(e)} className={'square'}></div>)
-        }
-      </S_GridContainer>
-      <button onClick={() => handleEightbyEight()}>8x8</button>
-      <button onClick={() => handleSixteenBySixteen()}>16x16</button>
-    </>
+      <S_Section>
+        <S_GridContainer grid={grid.length}>
+          {
+            grid.map((index) => <div key={index} onMouseEnter={(e) => changeBackground(e)} className={'square'}></div>)
+          }
+        </S_GridContainer>
+        <S_ButtonContainer>
+          <button onClick={() => handleEightbyEight()}>8x8</button>
+          <button onClick={() => handleSixteenBySixteen()}>16x16</button>
+        </S_ButtonContainer>
+      </S_Section>
+    </S_Page>
   )
 }
 
 export default Etchasketch
 
+const S_Page = styled.main`
+  width: 100%;
+  height: 100vh;
+
+  h1 {
+    margin: 0;
+  }
+`
+
+const S_Section = styled.div`
+  width: 100%;
+  margin: 1rem 0;
+`
+
 const S_GridContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-  width: 400px;
-  height: 400px;
+  width: 256px;
+  height: 256px;
+  margin: 0 auto 2rem auto;
 
   div {
-    width: ${(p: Props) => p.grid === 64 ? '47px' : p.grid === 256 ? '22px' : null};
-    height: ${(p: Props) => p.grid === 64 ? '47px' : p.grid === 256 ? '22px' : null};
+    width: ${(p: Props) => p.grid === 64 ? '30px' : p.grid === 256 ? '14px' : null};
+    height: ${(p: Props) => p.grid === 64 ? '30px' : p.grid === 256 ? '14px' : null};
     background: #777777;
     border: 1px solid #7b1919;
+  }
+`
+
+const S_ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+
+  button {
+    padding: 1rem;
+    margin-right: 1rem;
+    border-radius: 1rem;
+    border: none;
+    
   }
 `
