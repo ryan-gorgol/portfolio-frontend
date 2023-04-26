12cbbce8572660f9856e7a926bdeea928a99766b
Ryan Gorgol <ryangorgol@gmail.com>
2023-04-26 15:52:01 -0700

    fixed minor error causing build fail

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -4,7 +4,7 @@ import Page from '../components/Page'
 
 const dashboard = () => {
   return (
-    <Page>
+    <Page isOpen>
       <>
         {/* <Header title={'dashboard'} renderButton /> */}
         mpfc
