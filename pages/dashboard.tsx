40b3f87cbca5bafabc59c94a22919ab19b2bccc8
Ryan Gorgol <ryangorgol@gmail.com>
2023-04-26 19:19:09 -0700

    cleaning up in preparation for mlb Dash app

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import Header from '../components/Header'
 import Page from '../components/Page'
 
 const dashboard = () => {
   return (
     <Page isOpen>
       <>
-        {/* <Header title={'dashboard'} renderButton /> */}
-        mpfc
+
       </>
     </Page>
   )
